aa0c73def3d24e88ae3b6221c02669a33b75299c
Satyam Raj <rajsatyammm@gmail.com>
2024-12-19 15:25:36 +0530

    backend url changed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://notes-app-backend-p5ln.onrender.com',
+    baseURL: 'https://notes-app-backend-55ff.onrender.com/',
     // baseURL: 'http://localhost:5000',
 });
 
